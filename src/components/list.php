c8b6884d3671e0d814f52230078a1d57fe67dfd3
vitkolos <vit.kolos@gmail.com>
2022-03-21 15:05:28 +0100

    language list fixes

--- src/components/list.php
+++ src/components/list.php
@@ -30,14 +30,27 @@ function getStudentsTable() {
 
 function getLanguagesTable() {
     $html = '<table><thead><tr>
-    <th>id</th><th>jazyk</th><th>značka</th><th>třída</th><th>kapacita</th><th>upravit</th>
+    <th>id</th><th>jazyk</th><th>značka</th><th>třída</th><th>obsazeno/kapacita</th><th>upravit</th>
     </tr></thead><tbody>';
     $languagesTable = sql('SELECT * FROM `' . prefixTable('languages') . '`;');
 
     foreach ($languagesTable as $row) {
-        $html .= "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[4]</td><td>$row[2]</td><td>$row[3]</td><td><a href=\"?edit=language&id=$row[0]\">upravit</a></td></tr>";
+        $numberOfChoices = getLanguageOccupancy($row['class'], $row['id']);
+        $html .= "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[4]</td><td>$row[2]</td><td>$numberOfChoices/$row[3]</td><td><a href=\"?edit=language&id=$row[0]\">upravit</a></td></tr>";
     }
 
     $html .= '</tbody></table>';
     return $html;
+}
+
+function getLanguageOccupancy($class, $langId) {
+    $result = sql('SELECT COUNT(*) FROM `' . prefixTable('students') . '` WHERE `class`=? AND `choice`=?;', true, array($class, $langId));
+    $number = isset($result[0]) && isset($result[0][0]) ? intval($result[0][0]) : 0;
+    return $number;
+}
+
+function isLanguageAvailable($class, $langId) {
+    $result = sql('SELECT `limit` FROM `' . prefixTable('languages') . '` WHERE `id`=?;', true, array($langId));
+    $limit = isset($result[0]) && isset($result[0][0]) ? intval($result[0][0]) : 0;
+    return getLanguageOccupancy($class, $langId) < $limit;
 }
